5712c7f68672f1159c32fe911764fd15091f1b23
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2023-08-06 22:11:12 +0300

    Added an option to add a new todo

--- src/main/java/com/moeaj/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/moeaj/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,15 @@
 package com.moeaj.springboot.myfirstwebapp.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -24,6 +28,17 @@ public class TodoController {
         model.put("todos", todos);
 
         return "listTodos";
+    }
+
+    @RequestMapping(value="add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+    @RequestMapping(value="add-todo", method = RequestMethod.POST)
+    public String addNewTodo(ModelMap model, Todo todo) {
+        String username = (String)model.get("name");
+        todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+        return "redirect:list-todos";
     }
 
 }
